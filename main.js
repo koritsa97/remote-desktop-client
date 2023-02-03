71a2b209f55f5573f3cad2b7152dfbfa2617704f
koritsa97 <olia.korniv@gmail.com>
2023-02-03 19:13:09 +0200

    init'

---
+++ main.js
@@ -0,0 +1,17 @@
+import { io } from 'socket.io-client';
+import { throttle } from 'lodash';
+
+import './style.css';
+
+const controlElem = document.querySelector('.control');
+
+const socket = io(import.meta.env.VITE_SOCKET_URL);
+
+controlElem.onmousemove = throttle((e) => {
+  const coords = {
+    x: e.x / e.target.clientWidth,
+    y: e.y / e.target.clientHeight,
+  };
+
+  socket.emit('control', coords);
+}, 100);
