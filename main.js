f4cefcaaa28a6b0cc880f7f2e107caad78f9ee89
koritsa97 <olia.korniv@gmail.com>
2023-02-03 20:52:32 +0200

    added screen sharing

--- main.js
+++ main.js
@@ -1,11 +1,39 @@
 import { io } from 'socket.io-client';
 import { throttle } from 'lodash';
+import { Peer } from 'peerjs';
 
 import './style.css';
 
 const controlElem = document.querySelector('.control');
+const peerIdForm = document.querySelector('#peerIdForm');
 
 const socket = io(import.meta.env.VITE_SOCKET_URL);
+const peer = new Peer();
+
+peer.on('open', (id) => {
+  console.log(id);
+});
+
+peerIdForm.onsubmit = (e) => {
+  e.preventDefault();
+
+  const peerId = 'olia-remoter';
+  console.log(peerId);
+
+  navigator.mediaDevices
+    .getDisplayMedia({
+      audio: false,
+      video: true,
+    })
+    .then((mediaStream) => {
+      const call = peer.call(peerId, mediaStream);
+
+      call.on('stream', (stream) => {
+        console.log(stream);
+        controlElem.srcObject = stream;
+      });
+    });
+};
 
 controlElem.onmousemove = throttle((e) => {
   const coords = {
